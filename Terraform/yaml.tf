751598c4416c6aaff8c10228c1ae5145b29866fc
Korede <macbook@Koredes-MacBook-Pro.local>
2024-09-03 18:31:08 +0100

    testing

--- Terraform/yaml.tf
+++ Terraform/yaml.tf
@@ -13,3 +13,61 @@ resource "kubernetes_service_account" "ksa" {
     }
   }
 }
+
+resource "kubernetes_deployment" "shortlet_api" {
+  metadata {
+    name      = "shortlet-api"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "shortlet-api"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "shortlet-api"
+        }
+      }
+
+      spec {
+        service_account_name = "stackdriver-ksa"
+
+        container {
+          name  = "shortlet-api"
+          image = "gcr.io/genial-diagram-434320-c8/shortlet-api"
+
+          port {
+            container_port = 3000
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "shortlet_api" {
+  metadata {
+    name      = "shortlet-api"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "shortlet-api"
+    }
+
+    port {
+      port        = 80
+      target_port  = 300
+    }
+
+    type = "LoadBalancer"
+  }
+}
